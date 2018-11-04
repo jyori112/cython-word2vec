2e07a04106e6c8e8258bb9455da7c40bf3a0b9cf
jyori112 <jyori112@gmail.com>
2018-11-04 17:52:44 +0900

    Increase workers

--- shell.sh
+++ shell.sh
@@ -1,8 +1,12 @@
 DATA=/data/jsakuma/datasets
 ROOT=/data/jsakuma/sym_skipgram
 
-pipenv run python main.py train $DATA/europarl/en $ROOT/europarl/en.dic $ROOT/europarl/en.symmetric --symmetric
-pipenv run python main.py train $DATA/europarl/en $ROOT/europarl/en.dic $ROOT/europarl/en.no-symmetric --no-symmetric
+pipenv run python main.py train $DATA/europarl/en $ROOT/europarl/en.dic $ROOT/europarl/en.symmetric \
+    --symmetric --workers 16
+pipenv run python main.py train $DATA/europarl/en $ROOT/europarl/en.dic $ROOT/europarl/en.no-symmetric \
+    --no-symmetric --workers 16
 
-pipenv run python main.py train $DATA/wikipedia/en $ROOT/wikipedia/en.min10.dic $ROOT/wikipedia/en.symmetric --symmetric
-pipenv run python main.py train $DATA/wikipedia/en $ROOT/wikipedia/en.min10.dic $ROOT/wikipedia/en.no-symmetric --no-symmetric
+pipenv run python main.py train $DATA/wikipedia/en $ROOT/wikipedia/en.min10.dic $ROOT/wikipedia/en.symmetric \
+    --symmetric --workers 16
+pipenv run python main.py train $DATA/wikipedia/en $ROOT/wikipedia/en.min10.dic $ROOT/wikipedia/en.no-symmetric \
+    --no-symmetric --workers 16
