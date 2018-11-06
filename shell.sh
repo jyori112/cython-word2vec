a9fd5cb42fa03f40969e3295029b96aeefa89acb
jyori112 <jyori112@gmail.com>
2018-11-07 00:39:50 +0900

    Add shell script to train gensim model

--- shell.sh
+++ shell.sh
@@ -1,12 +1,14 @@
 DATA=/data/jsakuma/datasets
 ROOT=/data/jsakuma/sym_skipgram
 
-pipenv run python main.py train $DATA/europarl/en $ROOT/europarl/en.dic $ROOT/europarl/en.symmetric \
-    --symmetric --workers 16
-pipenv run python main.py train $DATA/europarl/en $ROOT/europarl/en.dic $ROOT/europarl/en.no-symmetric \
-    --no-symmetric --workers 16
-
-pipenv run python main.py train $DATA/wikipedia/en $ROOT/wikipedia/en.min10.dic $ROOT/wikipedia/en.symmetric \
-    --symmetric --workers 16
-pipenv run python main.py train $DATA/wikipedia/en $ROOT/wikipedia/en.min10.dic $ROOT/wikipedia/en.no-symmetric \
-    --no-symmetric --workers 16
+pipenv run python main.py train-gensim $DATA/europarl/en $ROOT/europarl/en.gensim --workers 16
+pipenv run python main.py train-gensim $DATA/wikipedia/en $ROOT/wikipedia/en.gensim --workers 16
+#pipenv run python main.py train $DATA/europarl/en $ROOT/europarl/en.dic $ROOT/europarl/en.symmetric \
+#    --symmetric --workers 16
+#pipenv run python main.py train $DATA/europarl/en $ROOT/europarl/en.dic $ROOT/europarl/en.no-symmetric \
+#    --no-symmetric --workers 16
+#
+#pipenv run python main.py train $DATA/wikipedia/en $ROOT/wikipedia/en.min10.dic $ROOT/wikipedia/en.symmetric \
+#    --symmetric --workers 16
+#pipenv run python main.py train $DATA/wikipedia/en $ROOT/wikipedia/en.min10.dic $ROOT/wikipedia/en.no-symmetric \
+#    --no-symmetric --workers 16
